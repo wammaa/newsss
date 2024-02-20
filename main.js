6b7572731f19be0219e6e3a8bf87d2092156c510
Hwiho Lee <lhh323@gmail.com>
2024-02-20 20:12:36 +0900

    2/20

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ const API_KEY = `ba6cff8a0208485f8e5d2cdcbf812b9a`;
 let news = [];
 
 const getLatestNews = async() =>{
-  const url = new URL(`http://times-node-env.eba-appvq3ef.ap-northeast-2.elasticbeanstalk.com/top-headlines?country=kr&apiKey=${API_KEY}`);
+  const url = new URL(`https://newswen.netlify.app//top-headlines?country=kr&apiKey=${API_KEY}`);
   const response = await fetch(url);
   const data = await response.json();
   news = data.articles;
