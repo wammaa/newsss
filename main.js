03301114b06d25341410b68cc8ac567b5849c445
Hwiho Lee <lhh323@gmail.com>
2024-02-22 20:46:09 +0900

    2/22

--- main.js
+++ main.js
@@ -1,5 +1,25 @@
 const API_KEY = `ba6cff8a0208485f8e5d2cdcbf812b9a`;
 let newsList = [];
+const menus = document.querySelectorAll('.menu-bar button');
+
+menus.forEach(menu=>menu.addEventListener("click", (event)=>getNewsByCategory(event)));
+
+const openNav = () => {
+  document.getElementById("mySidenav").style.width = "250px";
+};
+
+const closeNav = () => {
+  document.getElementById("mySidenav").style.width = "0";
+};
+
+const openSearchBox = () => {
+  let inputArea = document.getElementById("input-area");
+  if (inputArea.style.display === "inline") {
+    inputArea.style.display = "none";
+  } else {
+    inputArea.style.display = "inline";
+  }
+};
 
 const getLatestNews = async() =>{
   const url = new URL(`https://newswen.netlify.app//top-headlines?country=kr&apiKey=${API_KEY}`);
@@ -9,6 +29,27 @@ const getLatestNews = async() =>{
   render();
   console.log("rrrr", newsList);
 }
+
+const getNewsByCategory=async(event)=>{
+  const category = event.target.textContent.toLowerCase();
+  const url = new URL(`https://newswen.netlify.app//top-headlines?country=kr&category=${category}&apiKey=${API_KEY}`);
+  const response = await fetch(url);
+  const data = await response.json();
+  newsList = data.articles;
+  render();
+}
+
+const getNewByKeyword=async()=>{
+  const keyword = document.getElementById('search-input').value;
+  const url = new URL(`https://newswen.netlify.app//top-headlines?country=kr&q=${keyword}&apiKey=${API_KEY}`)
+  const response = await fetch(url);
+  const data = await response.json();
+  console.log("Aaaa", data)
+  newsList = data.articles;
+  render()
+}
+
+
 
 const render=()=>{
   const newsHTML = newsList.map(news=>`<div class="row news">
