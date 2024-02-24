15c3d82fea013484b74367728352d00fbcf005c9
Hwiho Lee <lhh323@gmail.com>
2024-02-24 21:45:02 +0900

    2/24

--- main.js
+++ main.js
@@ -5,6 +5,12 @@ let url = new URL(`https://newswen.netlify.app//top-headlines?country=kr&apiKey=
 
 menus.forEach(menu=>menu.addEventListener("click", (event)=>getNewsByCategory(event)));
 
+let totalResults = 0;
+let page = 1;
+const pageSize = 10;
+const groupSize = 5;
+
+
 const openNav = () => {
   document.getElementById("mySidenav").style.width = "250px";
 };
@@ -24,14 +30,19 @@ const openSearchBox = () => {
 
 const getNews=async()=>{
   try{
+    url.searchParams.set("page", page);
+    url.searchParams.set("pageSize", pageSize);
     const response = await fetch(url);
     const data = await response.json();
+    console.log("aaaa", data);
     if(response.status === 200){
       if(data.articles.length === 0){
         throw new Error("No result for this search")
       }
       newsList = data.articles;
+      totalResults = data.totalResults;
       render();
+      paginationRender();
     }else{
       throw new Error(data.message)
     }
@@ -74,7 +85,6 @@ const render=()=>{
     </div>
   </div>
 </div>`).join('');
-  console.log("news", newsHTML)
   document.getElementById('news-board').innerHTML=newsHTML;
 }
 
@@ -82,5 +92,28 @@ const errorRender=(errorMessage)=>{
   const errorHTML = `<div class="alert alert-danger" role="alert">
   ${errorMessage}</div>`
   document.getElementById('news-board').innerHTML=errorHTML;
+}
+
+const paginationRender=()=>{
+  const totalPages = Math.ceil(totalResults/pageSize);
+  const pageGroup = Math.ceil(page/groupSize);
+  let lastPage = pageGroup * groupSize;
+  if(lastPage > totalPages){
+    lastPage = totalPages;
+  }
+  const firstPage = lastPage - (groupSize - 1)<=0? 1:lastPage - (groupSize - 1);
+  let paginationHTML = `<li class="page-item" onclick="moveToPage(${page-1})"><a class="page-link" href="#">&lt;</a></li>`;
+
+  for(let i = firstPage; i<=lastPage; i++){
+    paginationHTML += `<li class="page-item ${i===page?"active":''}" onclick="moveToPage(${i})"><a class="page-link">${i}</a></li>`
+  }
+  paginationHTML += `<li class="page-item" onclick="moveToPage(${page+1})"><a class="page-link" href="#">&gt;</a></li>`;
+  document.querySelector(".pagination").innerHTML = paginationHTML;
+}
+
+const moveToPage=(pageNum)=>{
+  console.log("movetoPage", pageNum)
+  page = pageNum;
+  getNews();
 }
 getLatestNews();
